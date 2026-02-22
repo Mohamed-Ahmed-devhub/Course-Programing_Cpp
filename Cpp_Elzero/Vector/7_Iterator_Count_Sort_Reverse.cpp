890401b782dc74c2f017e769651ebeefc28311cf
Mohamed_Ahmed <mohamed.ahmed.programer@gmail.com>
2026-02-22 10:00:00 +0200

    feat: solve advanced logic problems

--- Cpp_Elzero/Vector/7_Iterator_Count_Sort_Reverse.cpp
+++ Cpp_Elzero/Vector/7_Iterator_Count_Sort_Reverse.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int main()
 {
-  vector<int> nums ={ 20,40,20,-80,70,20,50,20};
+  vector<int> nums ={ 20,90,20,-80,70,20,50,20};
   int val =20;
   int countTime = count(nums.begin(), nums.end(),val);
   cout << " Number " << val << " Found " <<  countTime << " Time\n ";
