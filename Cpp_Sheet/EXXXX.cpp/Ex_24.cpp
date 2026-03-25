f6c5f6ce6c2400795431f29bf711e12b8b90df38
Mohamed_Ahmed <mohamed.ahmed.programer@gmail.com>
2026-03-25 05:44:52 +0200

    add solve code 24 (5/9)∗(℉−32)

--- Cpp_Sheet/EXXXX.cpp/Ex_24.cpp
+++ Cpp_Sheet/EXXXX.cpp/Ex_24.cpp
@@ -1,3 +1,15 @@
 // 24. Printing the Fahrenheit-Celsius table for F = 0, 20, 30... 300. Use the 
 // following equation: 
 // ℃=(5/9)∗(℉−32) 
+#include <iostream>
+using namespace std;
+int main() 
+{
+  double result;
+  for(int i=0; i<=300; i+=10)
+  {
+    result = (5.0/9.0)*(i-32.0);
+    cout << i << " = " << result <<endl;
+  }
+  return 0;
+}
