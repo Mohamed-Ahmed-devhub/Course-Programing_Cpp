c4e85cd0d09e717d9474bfb5fdc78c9b440c3cf5
Mohamed_Ahmed <mohamed.ahmed.programer@gmail.com>
2026-03-25 03:12:40 +0200

    solve code Ex1 sheet

--- Cpp_Sheet/EXXXX.cpp/EX_1.cpp
+++ Cpp_Sheet/EXXXX.cpp/EX_1.cpp
@@ -3,3 +3,32 @@
 // he/she wants to make another calculation, if he/she enters ‘y’ the 
 // program read another two numbers and an operation, else the program 
 // ends.
+#include <iostream>
+using namespace std;
+int main() 
+{
+  int a, b;
+  char op;
+  char c;
+  do
+  {
+    cin >> a >> op >> b;
+    if(op=='+')
+    {
+      cout << a + b << "\n";
+    }
+    else if(op=='-')
+    {
+      cout << a - b << "\n";
+    }
+    else if(op=='*')
+    {
+      cout << a * b << "\n";
+    }
+    cout << "Anather Calc ?\n";
+    cin >> c ;
+  } while (c=='y');
+
+  
+  return 0;
+}
