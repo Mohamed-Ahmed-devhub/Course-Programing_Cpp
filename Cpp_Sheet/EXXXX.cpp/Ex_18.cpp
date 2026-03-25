89bb4c3facb2d54d535898e14e9e412d59433ed3
Mohamed_Ahmed <mohamed.ahmed.programer@gmail.com>
2026-03-25 03:34:26 +0200

    add solve code

--- Cpp_Sheet/EXXXX.cpp/Ex_18.cpp
+++ Cpp_Sheet/EXXXX.cpp/Ex_18.cpp
@@ -1,1 +1,16 @@
 // 18. Computing the sum of all integers from 1 to any given integer. 
+#include <iostream>
+using namespace std;
+int main() 
+{
+  int x,sum=0;
+  
+  cin >> x;
+  
+  for(int i=1; i<=x ; i++)
+  {
+    sum+=i;
+    cout << sum << "\n";
+  }
+  return 0;
+}
