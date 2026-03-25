175eaedbb07ec0c325bcba1bdc6033db92d76cc4
Mohamed_Ahmed <mohamed.ahmed.programer@gmail.com>
2026-03-25 04:32:09 +0200

    add solve code 21

--- Cpp_Sheet/EXXXX.cpp/Ex_21.cpp
+++ Cpp_Sheet/EXXXX.cpp/Ex_21.cpp
@@ -1,1 +1,25 @@
 // 21. Finding the maximum/minimum of a set of numbers. 
+#include <iostream>
+#include<climits>
+using namespace std;
+int main() 
+{
+  int n,x ;
+  cin >> n;
+  int min =   INT_MAX;
+  int max =   INT_MIN;
+  for(int i=1; i<=n; i++)
+  {
+    cin >> x;
+    if(x>max)
+    {
+      max = x;
+    }
+    if(x<min)
+    {
+      min = x;
+    }
+  }
+  cout << "THE MAX : " << max << "&&" << "THE MIN : " << min << "\n";
+  return 0;
+}
