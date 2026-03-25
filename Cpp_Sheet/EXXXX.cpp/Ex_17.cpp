401c14fd83ff843643917ff033d8df8f056d6f55
Mohamed_Ahmed <mohamed.ahmed.programer@gmail.com>
2026-03-25 03:22:08 +0200

    solve code

--- Cpp_Sheet/EXXXX.cpp/Ex_17.cpp
+++ Cpp_Sheet/EXXXX.cpp/Ex_17.cpp
@@ -1,3 +1,17 @@
 // 17. Consuming a natural number n and a symbol and producing a list with n 
 // occurrences of the symbol. 
 // Page 7 of 19 
+#include <iostream>
+using namespace std;
+int main() 
+{
+  int x;
+  char y;
+  cin >> x;
+  cin >> y;
+  for(int i=1; i<=5 ; i++)
+  {
+    cout << y;
+  }
+  return 0;
+}
