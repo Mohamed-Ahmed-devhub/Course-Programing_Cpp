fc195fafbbb8146130f91cf2884f004a25ce6ac0
Mohamed_Ahmed <mohamed.ahmed.programer@gmail.com>
2026-03-25 03:53:09 +0200

    solve code

--- Cpp_Sheet/EXXXX.cpp/Ex_20.cpp
+++ Cpp_Sheet/EXXXX.cpp/Ex_20.cpp
@@ -1,2 +1,16 @@
 // 20. Reading two integers x and y, then computing the sum of squares of 
 // integers from x to y. 
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main() 
+{
+  int x,y,sum=0;
+  cin >> x >> y ;
+  for(int i=1;i<=y;i++)
+  {
+    sum = sum + i*i;
+    
+  }cout << sum << "\n";
+  return 0;
+}
