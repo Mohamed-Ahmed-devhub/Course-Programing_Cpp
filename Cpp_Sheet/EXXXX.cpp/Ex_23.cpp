a47249e3dd175c976f485012c11e96674935b95b
Mohamed_Ahmed <mohamed.ahmed.programer@gmail.com>
2026-03-25 05:23:54 +0200

    solve code

--- Cpp_Sheet/EXXXX.cpp/Ex_23.cpp
+++ Cpp_Sheet/EXXXX.cpp/Ex_23.cpp
@@ -1,1 +1,14 @@
 // Computing the factorial of any given integer.
+#include <iostream>
+using namespace std;
+int main() 
+{
+  int x , fac=1;
+  cin >> x;
+  for(int i=1;i<=x;i++)
+  {
+    fac = fac * i;
+    cout << fac <<endl;
+  }
+  return 0;
+}
